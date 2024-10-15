71c3cdfed79ad51c83d8251ee3176ec56ed711d3
Eldohub Academy <academy@eldohub.co.ke>
2024-10-15 14:31:04 +0300

    header and hero section design

---
+++ server.js
@@ -0,0 +1,26 @@
+const express =  require("express")
+
+const app = express()
+app.use(express.static("public")) // serve static files -- redirect requests fro .css, img, .js files to a folder public
+
+app.get("/", (req,res)=>{
+    res.render("index.ejs")
+})
+app.get("/about", (req,res)=>{
+    res.render("about.ejs")
+})
+app.get("/contact", (req,res)=>{
+    res.render("contact.ejs")
+})
+app.get("/services", (req,res)=>{
+    res.render("services.ejs")
+})
+app.get("/booknow", (req,res)=>{
+    res.render("booknow.ejs")
+})
+// page not found
+app.get("*", (req,res)=>{
+    res.status(404).render("404.ejs")
+})
+// start
+app.listen(3000, (err)=>console.log("Server running on port 3000"))
